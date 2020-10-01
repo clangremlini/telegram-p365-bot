9ea214271943c6a06bdae3c0876926845d649d40
m4x3r <m4x3r1337@yandex.ru>
2020-10-01 18:56:00 +0400

    update porno365 mirror

--- p365.py
+++ p365.py
@@ -6,6 +6,6 @@ bot = telebot.TeleBot('BOT_TOKEN')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     num = random.randrange(1, 23750)
-    bot.send_message(message.chat.id, 'http://porno365.work/movie/' + str(num))
+    bot.send_message(message.chat.id, 'http://porno365.lol/movie/' + str(num))
 
 bot.polling()   
